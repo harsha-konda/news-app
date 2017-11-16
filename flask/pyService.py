8604297376606903bacc2c044e2b327571e6676d
Harsha Konda <hkonda@andrew.cmu.edu>
2017-11-16 00:00:13 -0500

    added elastic search flask scraping capabilities

--- flask/pyService.py
+++ flask/pyService.py
@@ -1,30 +1,68 @@
 from flask import Flask, request
-import sys
 import json
-import os, sys
 import newspaper
+from elasticsearch import Elasticsearch
+
+from datetime import datetime
 
 app = Flask(__name__, static_url_path='')
+es = Elasticsearch()
 
-@app.route("/", methods=['GET', 'POST'])
-def handle():
+elasticeEndPoint="http://localhost:9200/news"
 
-    if request.method == 'POST':
-        # Implementation goes here.
-        #
-        # Both stdout and stderr should be captured.
-        # {"stdout": "<output_from_stdout>", "stderr": "<output_from_stderr>"}
+@app.route("/generatePaper", methods=['GET'])
+def handle1():
+    if request.method == 'GET':
 
-        code=request.form['code']
+        url=request.args['url']
+        print(url)
+        paper=newspaper.build(url,memoize_articles=False)
 
 
-        final_dump=json.dumps({"stderr":code,"stdout":"dpg"})
+        articles=[]
+        i=0
+        for article in paper.articles:
 
-        return final_dump
+            article.download()
+            article.parse()
+            article.nlp()
+            articles+=[article]
+            i+=1
+            if i>100:
+                break
 
+            es.index(index='news', doc_type='post', body={
+                             "text":article.text,
+                             "keywords":article.keywords,
+                             "authors":article.authors,
+                             "summary":article.summary,
+                             "image":article.top_image,
+                             "link":url,
+                             "upvotes":0,
+                             "timestamp":datetime.now(),
+                             "comments":[]
+                    })
+
+
+
+        return json.dumps({"news":1})
+
+
+@app.route("/dropIndex",methods=['GET'])
+def handle2():
     if request.method=='GET':
+        index=request.args['index']
+        es.indices.delete(index=index, ignore=[400, 404])
+        return json.dumps({"success":1})
 
-        return json.dumps({"newspaper":"some randomshit"})
+
+@app.route("/createIndex", methods=['GET'])
+def handle3():
+    if request.method == 'GET':
+        index = request.args['index']
+        es.indices.create(index=index, ignore=[400, 404])
+        return json.dumps({"success": 1})
+
 
 if __name__ == '__main__':
     app.run(threaded=True, debug=True, host="0.0.0.0", port=5000)
